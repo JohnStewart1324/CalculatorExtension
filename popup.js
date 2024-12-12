bd45649eb0d6cae5608ea4f5f7922bf658856b26
john.stewart1324 <57651326+JohnStewart1324@users.noreply.github.com>
2024-12-11 23:57:58 -0800

    added functionality for basic operations and input memory

--- popup.js
+++ popup.js
@@ -1,4 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll(".btn");
 
+    let input = "";
+    let old = "";
+    let operator = null;
 
-})
+    //Button clicks handler
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const value = button.value;
+            console.log(value);
+            if (["+","-","*","/"].includes(value)) {
+                operator = value;
+                old = input;
+                input = "";
+                display.value = operator;
+            }
+
+            else if (value === "=") {
+                switch (operator) {
+                    case "+":
+                        display.value = Number(old) + Number(input);
+                    break;
+                    case "-":
+                        display.value = Number(old) - Number(input);
+                    break;
+                    case "*":
+                        display.value = Number(old) * Number(input);
+                    break;
+                    case "/":
+                        display.value = Number(old) / Number(input);
+                    break;
+                }
+            }
+
+            else if (value === "C") {
+                input = "";
+                old = "";
+                operator = null;
+                display.value = "";
+                
+            }
+
+            else {
+                input += value;
+                display.value = input;
+                
+            }
+        });
+    });
+});
